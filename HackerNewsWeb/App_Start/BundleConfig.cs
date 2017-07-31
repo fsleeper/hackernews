4a88b11f5705fa0c964350300ed48a77e4cc264f
FrankSleeper <frank.sleeper@Gmail.com>
2017-07-30 18:02:07 -0700

    Setting up Web APP

--- HackerNewsWeb/App_Start/BundleConfig.cs
+++ HackerNewsWeb/App_Start/BundleConfig.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Optimization;
+using Common.Logging;
 
 namespace HackerNewsWeb
 {
     public class BundleConfig
     {
+        private static readonly ILog Log = LogManager.GetLogger<BundleConfig>();
+
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            Log.Debug("Bundler Wiring Up");
+
+            // TBD if I need Angular UI Bootstrap
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                "~/Scripts/knockout-{version}.js",
-                "~/Scripts/knockout.validation.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/angular*.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                "~/Scripts/sammy-{version}.js",
-                "~/Scripts/app/common.js",
-                "~/Scripts/app/app.datamodel.js",
-                "~/Scripts/app/app.viewmodel.js",
-                "~/Scripts/app/home.viewmodel.js",
-                "~/Scripts/app/_run.js"));
+                "~/angular/app.js",
+                "~/angular/controller/*.js",
+                "~/angular/service/*.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -40,6 +41,7 @@ namespace HackerNewsWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/Content/bootstrap.css",
+                 "~/Content/bootstrap-theme.css",
                  "~/Content/Site.css"));
         }
     }
--- HackerNewsWeb/Global.asax.cs
+++ HackerNewsWeb/Global.asax.cs
@@ -8,12 +8,20 @@ namespace HackerNewsWeb
 {
     public class MvcApplication : HttpApplication
     {
+        // Yes, calling log4net.Config.XmlConfigurator.Configure(); is lame.  It's a bug in the current log4net universal with common logging
+
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
